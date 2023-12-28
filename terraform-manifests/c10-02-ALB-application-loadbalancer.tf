4405348c3692b35f378755f2a52811c051569659
AnonyIIMessiah <vaman1650.av@gmail.com>
2023-12-28 19:01:18 +0530

    c

--- terraform-manifests/c10-02-ALB-application-loadbalancer.tf
+++ terraform-manifests/c10-02-ALB-application-loadbalancer.tf
@@ -1,7 +1,8 @@
 # Terraform AWS Application Load Balancer (ALB)
 module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "6.0.0"
+  source = "terraform-aws-modules/alb/aws"
+  #version = "5.16.0"
+  version = "9.2.0"
 
   name               = "${local.name}-alb"
   load_balancer_type = "application"
@@ -9,28 +10,78 @@ module "alb" {
   subnets            = module.vpc.public_subnets
   security_groups    = [module.loadbalancer_sg.security_group_id]
 
-  # HTTP Listener - HTTP to HTTPS Redirect
-  http_tcp_listeners = [
-    {
-      port        = 80
-      protocol    = "HTTP"
-      action_type = "redirect"
+  # For example only
+  enable_deletion_protection = false
+
+  # Listeners
+  listeners = {
+    # Listener-1: my-http-https-redirect
+    my-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
       redirect = {
         port        = "443"
         protocol    = "HTTPS"
         status_code = "HTTP_301"
       }
-    }
-  ]
+    } # End my-http-https-redirect Listener
+
+    # Listener-2: my-https-listener
+    my-https-listener = {
+      port            = 443
+      protocol        = "HTTPS"
+      ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
+      certificate_arn = module.acm.acm_certificate_arn
+
+      # Fixed Response for Root Context 
+      fixed_response = {
+        content_type = "text/plain"
+        message_body = "Fixed Static message - for Root Context"
+        status_code  = "200"
+      } # End of Fixed Response
+
+      # Load Balancer Rules
+      rules = {
+        # Rule-1: myapp1-rule
+        myapp1-rule = {
+          actions = [{
+            type = "weighted-forward"
+            target_groups = [
+              {
+                target_group_key = "mytg1"
+                weight           = 1
+              }
+            ]
+            stickiness = {
+              enabled  = true
+              duration = 3600
+            }
+          }]
+          conditions = [{
+            path_pattern = {
+              values = ["/*"]
+            }
+          }]
+        } # End of myapp1-rule
+      }   # End Rules Block
+    }     # End my-https-listener Block
+  }       # End Listeners Block
+
   # Target Groups
-  target_groups = [
-    # App1 Target Group - TG Index = 0
-    {
-      name_prefix          = "app1-"
-      backend_protocol     = "HTTP"
-      backend_port         = 80
-      target_type          = "instance"
-      deregistration_delay = 10
+  target_groups = {
+    # Target Group-1: mytg1
+    mytg1 = {
+      # VERY IMPORTANT: We will create aws_lb_target_group_attachment resource separately when we use create_attachment = false, refer above GitHub issue URL.
+      ## Github ISSUE: https://github.com/terraform-aws-modules/terraform-aws-alb/issues/316
+      ## Search for "create_attachment" to jump to that Github issue solution
+      create_attachment                 = false
+      name_prefix                       = "mytg1-"
+      protocol                          = "HTTP"
+      port                              = 80
+      target_type                       = "instance"
+      deregistration_delay              = 10
+      load_balancing_cross_zone_enabled = false
+      protocol_version                  = "HTTP1"
       health_check = {
         enabled             = true
         interval            = 30
@@ -41,151 +92,25 @@ module "alb" {
         timeout             = 6
         protocol            = "HTTP"
         matcher             = "200-399"
-      }
-      protocol_version = "HTTP1"
-      # App1 Target Group - Targets
-      # targets = {
-      #   my_app1_vm1 = {
-      #     target_id = element([for instance in module.ec2_private_app1 : instance.id], 0)
-      #     port      = 80
-      #   },
-      #   my_app1_vm2 = {
-      #     target_id = element([for instance in module.ec2_private_app1 : instance.id], 1)
-      #     port      = 80
-      #   }
-      # }
-      tags = local.common_tags # Target Group Tags
-    },
-    # App2 Target Group - TG Index = 1
-    # {
-    #   name_prefix          = "app2-"
-    #   backend_protocol     = "HTTP"
-    #   backend_port         = 80
-    #   target_type          = "instance"
-    #   deregistration_delay = 10
-    #   health_check = {
-    #     enabled             = true
-    #     interval            = 30
-    #     path                = "/app2/index.html"
-    #     port                = "traffic-port"
-    #     healthy_threshold   = 3
-    #     unhealthy_threshold = 3
-    #     timeout             = 6
-    #     protocol            = "HTTP"
-    #     matcher             = "200-399"
-    #   }
-    #   protocol_version = "HTTP1"
-    #   # App2 Target Group - Targets
-    #   targets = {
-    #     my_app2_vm1 = {
-    #       target_id = element([for instance in module.ec2_private_app2 : instance.id], 0)
-    #       port      = 80
-    #     },
-    #     my_app2_vm2 = {
-    #       target_id = element([for instance in module.ec2_private_app2 : instance.id], 1)
-    #       port      = 80
-    #     }
-    #   }
-    #   tags = local.common_tags # Target Group Tags
-    # }
-  ]
-
-  # HTTPS Listener
-  https_listeners = [
-    # HTTPS Listener Index = 0 for HTTPS 443
-    {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = module.acm.acm_certificate_arn
-      action_type     = "fixed-response"
-      fixed_response = {
-        content_type = "text/plain"
-        message_body = "Fixed Static message - for Root Context"
-        status_code  = "200"
-      }
-    },
-  ]
+      }                        # End of Health Check Block
+      tags = local.common_tags # Target Group Tags 
+    }                          # END of Target Group-1: mytg1
+  }                            # END OF target_groups
+  tags = local.common_tags     # ALB Tags
+}                              # End of alb module
 
 
+resource "aws_lb_target_group_attachment" "external" {
+  # for_each = { for k, v in aws_instance.example : k => v }
 
-
-  # HTTPS Listener Rules
-  https_listener_rules = [
-    # Rule-1: custom-header=my-app-1 should go to App1 EC2 Instances
-    {
-      https_listener_index = 0
-      priority             = 1
-      actions = [
-        {
-          type               = "forward"
-          target_group_index = 0
-        }
-      ]
-      conditions = [{
-        path_patterns = ["/*"]
-        # #host_headers = [var.app1_dns_name]
-        # http_headers = [{
-        #   http_header_name = "custom-header"
-        #   values           = ["app-1", "app1", "my-app-1"]
-        # }]
-      }]
-    },
-    # # Rule-2: custom-header=my-app-2 should go to App2 EC2 Instances    
-    # {
-    #   https_listener_index = 0
-    #   priority             = 2
-    #   actions = [
-    #     {
-    #       type               = "forward"
-    #       target_group_index = 1
-    #     }
-    #   ]
-    #   conditions = [{
-    #     #path_patterns = ["/app2*"] 
-    #     #host_headers = [var.app2_dns_name]
-    #     http_headers = [{
-    #       http_header_name = "custom-header"
-    #       values           = ["app-2", "app2", "my-app-2"]
-    #     }]
-    #   }]
-    # },
-    # # Rule-3: When Query-String, website=aws-eks redirect to https://stacksimplify.com/aws-eks/
-    # {
-    #   https_listener_index = 0
-    #   priority             = 3
-    #   actions = [{
-    #     type        = "redirect"
-    #     status_code = "HTTP_302"
-    #     host        = "stacksimplify.com"
-    #     path        = "/aws-eks/"
-    #     query       = ""
-    #     protocol    = "HTTPS"
-    #   }]
-    #   conditions = [{
-    #     query_strings = [{
-    #       key   = "website"
-    #       value = "aws-eks"
-    #     }]
-    #   }]
-    # },
-    # # Rule-4: When Host Header = azure-aks.devopsincloud.com, redirect to https://stacksimplify.com/azure-aks/azure-kubernetes-service-introduction/
-    # {
-    #   https_listener_index = 0
-    #   priority             = 4
-    #   actions = [{
-    #     type        = "redirect"
-    #     status_code = "HTTP_302"
-    #     host        = "stacksimplify.com"
-    #     path        = "/azure-aks/azure-kubernetes-service-introduction/"
-    #     query       = ""
-    #     protocol    = "HTTPS"
-    #   }]
-    #   conditions = [{
-    #     host_headers = ["azure-aks101.devopsincloud.com"]
-    #   }]
-    # },
-  ]
-
-
-  tags = local.common_tags # ALB Tags
+  target_group_arn = module.alb.target_groups["mygt1"].arn
+  target_id        = module.alb.target_groups["mygt1"].id
+  port             = 80
 }
+
+# resource "aws_instance" "example" {
+#   for_each = toset(["1"]) # ¯\_(ツ)_/¯
+
+#   ami           = data.aws_ami.amzlinux2.id
+#   instance_type = var.instance_type
+# }
