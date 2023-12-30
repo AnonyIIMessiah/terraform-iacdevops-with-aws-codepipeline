9c8eac9b7c162d5960a2b1a11d56fce71ca2b063
AnonyIIMessiah <vaman1650.av@gmail.com>
2023-12-30 11:28:55 +0530

    check

--- terraform-manifests/c13-07-autoscaling-scheduled-actions.tf
+++ terraform-manifests/c13-07-autoscaling-scheduled-actions.tf
@@ -2,7 +2,7 @@
 ### Create Scheduled Action-1: Increase capacity during business hours
 resource "aws_autoscaling_schedule" "increase_capacity_7am" {
   scheduled_action_name  = "increase-capacity-7am"
-  min_size               = 2
+  min_size               = 3
   max_size               = 10
   desired_capacity       = 8
   start_time             = "2030-03-30T11:00:00Z" # Time should be provided in UTC Timezone (11am UTC = 7AM EST)
@@ -12,9 +12,9 @@ resource "aws_autoscaling_schedule" "increase_capacity_7am" {
 ### Create Scheduled Action-2: Decrease capacity during business hours
 resource "aws_autoscaling_schedule" "decrease_capacity_5pm" {
   scheduled_action_name  = "decrease-capacity-5pm"
-  min_size               = 2
+  min_size               = 3
   max_size               = 10
-  desired_capacity       = 2
+  desired_capacity       = 3
   start_time             = "2030-03-30T21:00:00Z" # Time should be provided in UTC Timezone (9PM UTC = 5PM EST)
   recurrence             = "00 21 * * *"
   autoscaling_group_name = aws_autoscaling_group.my_asg.id
