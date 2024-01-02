a440d4945133929b715fdb261743016bbb9876a1
AnonyIIMessiah <vaman1650.av@gmail.com>
2024-01-02 21:29:19 +0530

    check

---
+++ terraform-manifests/c13-01-sns-notifications.tf
@@ -0,0 +1,14 @@
+
+## SNS - Topic
+resource "aws_sns_topic" "my_sns_topic" {
+  #name = "myasg-sns-topic-${random_pet.this.id}"
+  name = "${local.name}-${random_pet.this.id}"
+}
+
+# # SNS - Subscription
+resource "aws_sns_topic_subscription" "my_sns_topic_subscription" {
+  topic_arn = aws_sns_topic.my_sns_topic.arn
+  protocol  = "email"
+  endpoint  = "vaman1650@gmail.com"
+}
+
--- terraform-manifests/c5-06-securitygroup-rdsdbsg.tf
+++ terraform-manifests/c5-06-securitygroup-rdsdbsg.tf
@@ -1,15 +1,11 @@
-# Security Group for AWS RDS DB
 module "rdsdb_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  #version = "3.18.0"
-  #version = "4.0.0"
   version = "5.1.0"  
 
   name        = "rdsdb-sg"
   description = "Access to MySQL DB for entire VPC CIDR Block"
   vpc_id      = module.vpc.vpc_id
 
-  # ingress
   ingress_with_cidr_blocks = [
     {
       from_port   = 3306
@@ -19,7 +15,7 @@ module "rdsdb_sg" {
       cidr_blocks = module.vpc.vpc_cidr_block
     },
   ]
-  # Egress Rule - all-all open
+
   egress_rules = ["all-all"]  
   tags = local.common_tags  
 }
